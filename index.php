285b3f9b03261a9ba27ad9949ccb9ab4e68cc1c1
TriYudoWibisono <dodolgggg@gmail.com>
2022-10-25 11:33:35 +0700

    menambahkan file index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>coba git</title>
+</head>
+
+<body>
+    <h1>upload banyak file ke github</h1>
+
+    <p>ini adalah commit-commit di git lokal yang akan kita simpan ke github</p>
+</body>
+
+</html>
